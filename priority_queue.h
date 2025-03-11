0f10579147354819d8cd410cb0f13758346d4d33
Zorro Savage <zorrosavage21@gmail.com>
2025-03-11 13:03:10 -0600

    top works, push/pop stubbed out

--- priority_queue.h
+++ priority_queue.h
@@ -87,11 +87,14 @@ public:
    //
    size_t size()  const 
    { 
-      return 99;   
+      return container.size();   
    }
    bool empty() const 
    { 
-      return false;  
+      if (container.size() > 0 && container.capacity() > 0)
+         return false;
+      else
+         return true;
    }
    
 private:
@@ -111,7 +114,11 @@ private:
 template <class T, class Container, class Compare>
 const T & priority_queue <T, Container, Compare> :: top() const
 {
-   return *(new T);
+   if (!container.empty()) // test to see if exeption needs thrown
+      return container.front();
+   else
+      throw std::out_of_range("std:out_of_range"); 
+
 }
 
 /**********************************************
@@ -121,6 +128,12 @@ const T & priority_queue <T, Container, Compare> :: top() const
 template <class T, class Container, class Compare>
 void priority_queue <T, Container, Compare> :: pop()
 {
+   // stubbed out the best I could does not compile
+   /* T* temp = container[0];
+   container[0] = container[size() - 1];
+   container[size() - 1] = temp;
+   container.pop_back();
+   percolateDown(); */
 }
 
 /*****************************************
@@ -130,10 +143,20 @@ void priority_queue <T, Container, Compare> :: pop()
 template <class T, class Container, class Compare>
 void priority_queue <T, Container, Compare> :: push(const T & t)
 {
+   // stubbed out the best I could does not compile
+   //container.push_back(t);
+   size_t i = size() / 2;
+   while (i && percolateDown(i))
+      i /= 2;
 }
 template <class T, class Container, class Compare>
 void priority_queue <T, Container, Compare> :: push(T && t)
 {
+   // stubbed out the best I could does not compile
+   //container->push_back(t);
+   size_t i = size() / 2;
+   while (i && percolateDown(i))
+      i /= 2;
 }
 
 /************************************************
